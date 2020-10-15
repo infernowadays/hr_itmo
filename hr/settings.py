62fb2cb8f3875504b8e97a7c8ac691a787165f15
maxim <maximus1998g@mail.ru>
2020-10-15 23:25:57 +0300

    Update settings.py

--- hr/settings.py
+++ hr/settings.py
@@ -128,3 +128,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # location where you will store your static files
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'project_name/static')
                     ]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
