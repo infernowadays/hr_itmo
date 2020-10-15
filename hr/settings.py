360b08fd2c797dda0018837e65682a2a2ed25653
maxim <maximus1998g@mail.ru>
2020-10-15 21:24:17 +0300

    set allowed hosts for heroku

--- hr/settings.py
+++ hr/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=True)
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'nameofapp.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'hr-itmo.herokuapp.com']
 
 # Application definition
 
