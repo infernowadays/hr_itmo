e2a3e67dc2a03d732cee07b17f84136356cd2946
maxim <maximus1998g@mail.ru>
2020-10-15 23:25:37 +0300

    Update settings.py

--- hr/settings.py
+++ hr/settings.py
@@ -122,3 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# location where django collect all static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# location where you will store your static files
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'project_name/static')
+                    ]
