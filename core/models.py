a3fe2a8a9d9cd4594de233b378d25277f60da5f9
maxim <maximus1998g@mail.ru>
2020-10-21 22:51:13 +0300

    fix models

--- core/models.py
+++ core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class University(models.Model):
-    name = models.CharField(max_length=64, null=False, blank=False)
+    name = models.CharField(max_length=128, null=False, blank=False)
 
     class Meta:
         db_table = 'university'
@@ -10,8 +10,8 @@ class University(models.Model):
 
 class Specialization(models.Model):
     code = models.CharField(max_length=64, null=False, blank=False)
-    name = models.CharField(max_length=64, null=False, blank=False)
-    qualification = models.CharField(max_length=64, null=False, blank=False)
+    name = models.CharField(max_length=128, null=False, blank=False)
+    qualification = models.CharField(max_length=128, null=False, blank=False)
 
     class Meta:
         db_table = 'specialization'
