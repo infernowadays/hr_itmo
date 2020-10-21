bf65bf55977b7e93e365d9cff030b0ca3e9a6073
maxim <maximus1998g@mail.ru>
2020-10-21 22:59:35 +0300

    fix serializers

--- core/serializers.py
+++ core/serializers.py
@@ -5,8 +5,10 @@ from .models import University, Specialization
 class UniversitySerializer(ModelSerializer):
     class Meta:
         model = University
+        fields = '__all__'
 
 
 class SpecializationSerializer(ModelSerializer):
     class Meta:
         model = Specialization
+        fields = '__all__'
