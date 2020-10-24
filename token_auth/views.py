4fe3382e347aab59c071bb0a536ba5f855b590a4
maxim <maximus1998g@mail.ru>
2020-10-24 17:49:57 +0300

    favourites

--- token_auth/views.py
+++ token_auth/views.py
@@ -67,6 +67,12 @@ class LoginView(APIView):
                 return Response({'error': 'user does not have any companies'}, status=status.HTTP_404_NOT_FOUND)
             serializer = CompanySerializer(company[0])
 
+        elif self.request.user.type == Type.ADMINISTRATOR.value:
+            company = Company.objects.filter(hr=self.request.user)
+            if not company:
+                return Response({'error': 'user does not have any companies'}, status=status.HTTP_404_NOT_FOUND)
+            serializer = CompanySerializer(company, many=True)
+
         elif self.request.user.type == Type.STUDENT.value:
             serializer = UserProfileSerializer(self.request.user)
 
